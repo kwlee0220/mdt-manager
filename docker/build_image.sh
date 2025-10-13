fb24ae09e10d3d938452fa4449cd3c512f12f825
Kang-Woo Lee <kwlee@etri.re.kr>
2025-10-13 12:33:03 +0900

    .

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -7,12 +7,9 @@ docker image rmi -f kwlee0220/mdt-manager:$VERSION
 
 cp $MDT_MANAGER_HOME/mdt-manager-all.jar mdt-manager-all.jar
 cp $MDT_MANAGER_HOME/mdt-instance-all.jar mdt-instance-all.jar
-cp $MDT_MANAGER_HOME/application.yml application.yml
-cp $MDT_MANAGER_HOME/logback.xml logback.xml
 
 docker build -t kwlee0220/mdt-manager:$VERSION .
 # docker push kwlee0220/mdt-client:latest
 
 rm mdt-manager-all.jar
-rm application.yml
-rm logback.xml
+rm mdt-instance-all.jar
