c8e202dde44ddc0b07fc8c3f9a0b8785e5ce09c4
Kang-Woo Lee <kwlee@etri.re.kr>
2025-10-31 10:37:25 +0900

    version 변수 이름 수정

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 MDT_MANAGER_HOME=$MDT_HOME/mdt-manager
-VERSION=1.2.0
+MDT_BUILD_VERSION=1.2.1
 
-docker image rmi -f kwlee0220/mdt-manager:$VERSION
+docker image rmi -f kwlee0220/mdt-manager:$MDT_BUILD_VERSION
 
 cp $MDT_MANAGER_HOME/mdt-manager-all.jar mdt-manager-all.jar
 cp $MDT_MANAGER_HOME/mdt-instance-all.jar mdt-instance-all.jar
 
-docker build -t kwlee0220/mdt-manager:$VERSION .
-# docker push kwlee0220/mdt-client:latest
+docker build -t kwlee0220/mdt-manager:$MDT_BUILD_VERSION .
+# docker push kwlee0220/mdt-manager:$MDT_BUILD_VERSION
 
 rm mdt-manager-all.jar
 rm mdt-instance-all.jar
