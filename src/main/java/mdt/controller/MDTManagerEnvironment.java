0a4b18776ba363381ad306e3132cce90ef0cfc64
Kang-Woo Lee <kwlee@etri.re.kr>
2025-11-10 11:56:10 +0900

    Refactor environment variable handling by replacing EnvironmentVariablePostProcessor with MDTManagerEnvironment

--- src/main/java/mdt/controller/MDTManagerEnvironment.java
+++ src/main/java/mdt/controller/MDTManagerEnvironment.java
@@ -2,6 +2,7 @@ package mdt.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -23,44 +24,50 @@ import utils.stream.KeyValueFStream;
  *
  * @author Kang-Woo Lee (ETRI)
  */
-public class EnvironmentVariablePostProcessor implements EnvironmentPostProcessor, Ordered {
+public class MDTManagerEnvironment implements EnvironmentPostProcessor, Ordered {
     private static final DeferredLog s_deferredLog = new DeferredLog();
-	
+    
 	private static final String ENV_FILE_ENV_VAR = "ENV_FILE";
 	private static final String ENV_FILE_PROPERTY = "env.file";
 	private static final String ENV_FILE_NAME = "config/env.file";
 	
 	private static Map<String,Object> s_environmentVariables = Map.of();
 	
-	public static Map<String,Object> getEnvironmentVariables() {
-		return s_environmentVariables;
+	public static Map<String,Object> getVariables() {
+		return Collections.unmodifiableMap(s_environmentVariables);
 	}
 	
 	@Override
 	public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication application) {
+        application.addListeners((ApplicationListener<ApplicationPreparedEvent>) event -> {
+        	Log actualLogger = LogFactory.getLog(MDTManagerEnvironment.class);
+			s_deferredLog.replayTo(actualLogger);
+		});
+        
 		String fromProp = System.getProperty(ENV_FILE_PROPERTY);
 		String fromEnv = System.getenv(ENV_FILE_ENV_VAR);
 		String path = firstNonBlank(fromProp, fromEnv, ENV_FILE_NAME);
 		
+		File envFile = new File(path);
+		if ( !envFile.exists() ) {
+			s_deferredLog.info("no environment file found at: " + envFile.getAbsolutePath());
+			return;
+		}
+
+		s_deferredLog.info("loading environment variables from file: " + envFile.getAbsolutePath());
 		try {
-			s_deferredLog.info("loading environment variables from file: " + path);
-			
 			EnvironmentFileLoader envLoader = EnvironmentFileLoader.from(new File(path));
 			LinkedHashMap<String, String> variables = envLoader.load();
 			
-			s_environmentVariables = KeyValueFStream.from(variables)
-													.peek(v -> s_deferredLog.info(String.format("  - EnvVar: %s=%s", v.key(), v.value())))
-													.mapValue(v -> (Object)v)
-													.toMap();
+			s_environmentVariables
+					= KeyValueFStream.from(variables)
+									.peek(v -> s_deferredLog.info(String.format("  - EnvVar: %s=%s", v.key(), v.value())))
+									.mapValue(v -> (Object)v)
+									.toMap();
 			MapPropertySource source = new MapPropertySource("mdtEnv", s_environmentVariables);
 			env.getPropertySources().addFirst(source);
 		}
 		catch ( IOException e ) { }
-        
-        application.addListeners((ApplicationListener<ApplicationPreparedEvent>) event -> {
-        	Log actualLogger = LogFactory.getLog(EnvironmentVariablePostProcessor.class);
-			s_deferredLog.replayTo(actualLogger);
-		});
 	}
 	
 	@Override
